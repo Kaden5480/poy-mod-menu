b50a41ad1fc81eeed078a933d17611a05f5e958c
Kaden5480 <kaden5480@protonmail.com>
2025-12-24 23:45:22 +0000

    Simplify bundle loading

--- src/Resources.cs
+++ src/Resources.cs
@@ -1,6 +1,6 @@
-using System.IO;
 using System.Reflection;
 
+using UILib;
 using UnityEngine;
 
 namespace ModMenu {
@@ -11,7 +11,7 @@ namespace ModMenu {
      * </summary>
      */
     internal static class Resources {
-        private const string bundlePath = "modmenu.bundle";
+        private const string bundlePath = "res.modmenu.bundle";
         private static AssetBundle bundle;
 
         /**
@@ -19,45 +19,21 @@ namespace ModMenu {
          * The default checkmark used by Peaks of Yore.
          * </summary>
          */
-        public static Texture2D searchGlass { get; private set; } = LoadFromBundle<Texture2D>(
+        public static Texture2D searchGlass { get; private set; } = LoadAsset<Texture2D>(
             "SearchGlass"
         );
 
         /**
          * <summary>
-         * Loads a file with the specified filename
-         * into a byte array.
-         *
-         * The files are loaded from res/, so passing
-         * a name of "image.png" will load res/image.png.
-         * </summary>
-         * <param name="name">The name of the file to load</param>
-         * <returns>The file's bytes</returns>
-         */
-        internal static byte[] LoadBytes(string name) {
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            string assemblyName = assembly.GetName().Name;
-
-            using (Stream stream = assembly.GetManifestResourceStream(
-                $"{assemblyName}.res.{name}"
-            )) {
-            using (MemoryStream mem = new MemoryStream()) {
-                stream.CopyTo(mem);
-                return mem.ToArray();
-            }}
-        }
-
-        /**
-         * <summary>
-         * Loads an asset by name.
+         * Mod Menu bundle loading.
          * </summary>
          * <param name="name">The name of the asset to load</param>
          * <returns>The loaded asset</returns>
          */
-        internal static T LoadFromBundle<T>(string name) where T : UnityEngine.Object {
+        private static T LoadAsset<T>(string name) where T : UnityEngine.Object {
             if (bundle == null) {
-                byte[] bundleData = LoadBytes(bundlePath);
-                bundle = AssetBundle.LoadFromMemory(bundleData);
+                bundle = UILib.Resources
+                    .LoadBundle(Assembly.GetExecutingAssembly(), bundlePath);
             }
 
             return bundle.LoadAsset<T>(name);
